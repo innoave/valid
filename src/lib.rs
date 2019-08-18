cd7ae7c23311c81897a710a11e257b25cf4a3469
Harald Maida <harald.maida@innoave.com>
2019-08-18 09:51:27 +0200

    setup project

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+#![doc(html_root_url = "https://docs.rs/valid/0.1.0")]
+#![warn(
+    bare_trait_objects,
+    missing_copy_implementations,
+    missing_debug_implementations,
+//    missing_docs,
+    rust_2018_idioms,
+    trivial_casts,
+    trivial_numeric_casts,
+    unsafe_code,
+    unstable_features,
+    unused_extern_crates,
+    unused_import_braces,
+    unused_qualifications
+)]
+#![allow(dead_code)] //TODO remove eventually
