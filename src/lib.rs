b54277e5c54c017641d06ece960be83d00292793
Harald Maida <harald.maida@innoave.com>
2019-08-18 10:54:21 +0200

    first ideas implemented

--- src/lib.rs
+++ src/lib.rs
@@ -14,3 +14,140 @@
     unused_qualifications
 )]
 #![allow(dead_code)] //TODO remove eventually
+
+#[cfg(feature = "bigdecimal")]
+use bigdecimal::BigDecimal;
+#[cfg(feature = "chrono")]
+use chrono::{DateTime, NaiveDate, Utc};
+#[cfg(feature = "serde1")]
+use serde::{Deserialize, Serialize};
+use std::borrow::Cow;
+
+#[cfg_attr(feature = "serde1", derive(Serialize, Deserialize))]
+#[derive(Debug, Clone, PartialEq)]
+pub enum Value {
+    String(String),
+    Integer(i32),
+    Long(i64),
+    Float(f32),
+    Double(f64),
+    #[cfg(feature = "bigdecimal")]
+    Decimal(BigDecimal),
+    #[cfg(feature = "chrono")]
+    Date(NaiveDate),
+    #[cfg(feature = "chrono")]
+    DateTime(DateTime<Utc>),
+}
+
+#[cfg_attr(feature = "serde1", derive(Serialize, Deserialize))]
+#[derive(Debug, Clone, PartialEq)]
+pub struct Field {
+    pub name: Cow<'static, str>,
+    pub value: Value,
+}
+
+#[cfg_attr(feature = "serde1", derive(Serialize, Deserialize))]
+#[derive(Debug, Clone, PartialEq)]
+pub struct OptionalField {
+    pub name: Cow<'static, str>,
+    pub value: Option<Value>,
+}
+
+#[cfg_attr(feature = "serde1", derive(Serialize, Deserialize))]
+#[derive(Debug, Clone, PartialEq)]
+pub struct InvalidValue {
+    pub code: Cow<'static, str>,
+    pub field: Field,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct InvalidRelation {
+    pub code: Cow<'static, str>,
+    pub field1: Field,
+    pub field2: OptionalField,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct InvalidState {
+    pub code: Cow<'static, str>,
+    pub params: Vec<Field>,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum ConstraintViolation {
+    Field(Field),
+    Relation(Field, Field),
+    State(InvalidState),
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct ValidationError {
+    pub message: Option<Cow<'static, str>>,
+    pub violations: Vec<ConstraintViolation>,
+}
+
+impl ValidationError {
+    pub fn merge(mut self, other: ValidationError) -> Self {
+        self.message = match (self.message, other.message) {
+            (_, Some(msg2)) => Some(msg2),
+            (Some(msg1), None) => Some(msg1),
+            (None, None) => None,
+        };
+        self.violations.extend(other.violations);
+        self
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub enum Validation<T> {
+    Successful(T),
+    Failed(ValidationError),
+}
+
+impl<T> Validation<T> {
+    pub fn result(self) -> Result<Validated<T>, ValidationError> {
+        match self {
+            Validation::Successful(entity) => Ok(Validated(entity)),
+            Validation::Failed(error) => Err(error),
+        }
+    }
+
+    pub fn and<U>(self, validate: impl Validate<Entity = U>, entity: &U) -> Validation<()> {
+        let validation2 = validate.validate(entity);
+        match self {
+            Validation::Successful(_) => match validation2 {
+                Validation::Successful(_) => Validation::Successful(()),
+                Validation::Failed(error2) => Validation::Failed(error2),
+            },
+            Validation::Failed(error) => match validation2 {
+                Validation::Successful(_) => Validation::Successful(()),
+                Validation::Failed(error2) => Validation::Failed(error.merge(error2)),
+            },
+        }
+    }
+
+    pub fn and_then<U>(self, validate: impl Validate<Entity = U>, entity: &U) -> Validation<U> {
+        match self {
+            Validation::Successful(_) => validate.validate(entity),
+            Validation::Failed(error) => Validation::Failed(error),
+        }
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct Validated<T>(T);
+
+impl<T> Validated<T> {
+    pub fn unwrap(self) -> T {
+        self.0
+    }
+}
+
+pub trait Validate {
+    type Entity;
+
+    fn validate(&self, entity: &Self::Entity) -> Validation<Self::Entity>;
+}
+
+#[cfg(test)]
+mod tests;
---
+++ src/tests.rs
@@ -0,0 +1,97 @@
+use super::*;
+
+mod value {
+    use super::*;
+
+    #[cfg(not(any(feature = "bigdecimal", feature = "chrono")))]
+    #[test]
+    fn exhaustive_match_over_value_variants_for_default_features() {
+        fn exhaustive_match(value: Value) -> i32 {
+            match value {
+                Value::String(_) => 1,
+                Value::Integer(_) => 2,
+                Value::Long(_) => 3,
+                Value::Float(_) => 4,
+                Value::Double(_) => 5,
+            }
+        }
+        assert_eq!(exhaustive_match(Value::Integer(0)), 2);
+    }
+
+    #[cfg(all(feature = "bigdecimal", not(feature = "chrono")))]
+    #[test]
+    fn exhaustive_match_over_value_variants_with_bigdecimal_feature() {
+        fn exhaustive_match(value: Value) -> i32 {
+            match value {
+                Value::String(_) => 1,
+                Value::Integer(_) => 2,
+                Value::Long(_) => 3,
+                Value::Float(_) => 4,
+                Value::Double(_) => 5,
+                Value::Decimal(_) => 6,
+            }
+        }
+        assert_eq!(exhaustive_match(Value::Integer(0)), 2);
+    }
+
+    #[cfg(all(not(feature = "bigdecimal"), feature = "chrono"))]
+    #[test]
+    fn exhaustive_match_over_value_variants_with_chrono_feature() {
+        fn exhaustive_match(value: Value) -> i32 {
+            match value {
+                Value::String(_) => 1,
+                Value::Integer(_) => 2,
+                Value::Long(_) => 3,
+                Value::Float(_) => 4,
+                Value::Double(_) => 5,
+                Value::Date(_) => 7,
+                Value::DateTime(_) => 8,
+            }
+        }
+        assert_eq!(exhaustive_match(Value::Integer(0)), 2);
+    }
+
+    #[cfg(all(feature = "bigdecimal", feature = "chrono"))]
+    #[test]
+    fn exhaustive_match_over_value_variants_with_bigdecimal_and_chrono_features() {
+        fn exhaustive_match(value: Value) -> i32 {
+            match value {
+                Value::String(_) => 1,
+                Value::Integer(_) => 2,
+                Value::Long(_) => 3,
+                Value::Float(_) => 4,
+                Value::Double(_) => 5,
+                Value::Decimal(_) => 6,
+                Value::Date(_) => 7,
+                Value::DateTime(_) => 8,
+            }
+        }
+        assert_eq!(exhaustive_match(Value::Integer(0)), 2);
+    }
+}
+
+mod validation {
+    use super::*;
+
+    #[test]
+    fn validation_of_type_that_does_not_implement_partial_eq() {
+        struct TypeWithoutPartialEqImpl(i32);
+
+        let value = Validation::Successful(TypeWithoutPartialEqImpl(42));
+
+        match value {
+            Validation::Successful(val) => assert_eq!(val.0, 42),
+            Validation::Failed(_) => {}
+        }
+    }
+
+    #[test]
+    fn validation_of_type_that_implements_partial_eq() {
+        #[derive(Debug, PartialEq)]
+        struct TypeWithPartialEqImpl(i32);
+
+        let value = Validation::Successful(TypeWithPartialEqImpl(42));
+
+        assert_eq!(value, Validation::Successful(TypeWithPartialEqImpl(42)));
+    }
+}
